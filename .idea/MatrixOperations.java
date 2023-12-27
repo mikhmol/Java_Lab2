8bffafcb86f41884a94f47b8258f3a13e520f046
mikhmol <mikhailo.moliakov@gmail.com>
2023-12-27 23:45:10 +0200

    Correct printMinElements method

--- .idea/MatrixOperations.java
+++ .idea/MatrixOperations.java
@@ -9,11 +9,6 @@ public class MatrixOperations {
     }
 
     public static void executeMatrixOperations() throws Exception {
-        int rowsA = 3;
-        int colsA = 3;
-        int rowsB = 3;
-        int colsB = 3;
-
         short[][] matrixA = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         short[][] matrixB = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
 
@@ -66,23 +61,13 @@ public class MatrixOperations {
         int cols = matrix[0].length;
 
         for (int j = 0; j < cols; j++) {
-            short minElement = 0;
-            boolean firstNonZeroFound = false;
-
-            for (int i = 0; i < matrix.length; i++) {
-                if (matrix[i][j] != 0) {
-                    if (!firstNonZeroFound || matrix[i][j] < minElement) {
-                        minElement = matrix[i][j];
-                        firstNonZeroFound = true;
-                    }
+            short minElement = matrix[0][j];
+            for (int i = 1; i < matrix.length; i++) {
+                if (matrix[i][j] < minElement) {
+                    minElement = matrix[i][j];
                 }
             }
-
-            if (firstNonZeroFound) {
-                System.out.println("Стовпець " + (j + 1) + ": " + minElement);
-            } else {
-                System.out.println("Стовпець " + (j + 1) + ": усі елементи стовпця рівні 0");
-            }
+            System.out.println("Стовпець " + j + ": " + minElement);
         }
     }
 }
